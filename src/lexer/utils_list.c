13649321ced4d65d45c63b92357779ce532845d6
Wenyan Lin <wennyan.lin@gmail.com>
2024-06-06 16:20:13 +0200

    some psudocode for tokenizing input

---
+++ src/lexer/utils_list.c
@@ -0,0 +1,13 @@
+#include "minishell.h"
+
+//1- get the 'word'
+//   when find the character that is not quotes and spaces, substracts it 
+//2- initialize a node with it's properties
+//3- add the node into a linked list 
+
+
+
+t_token **token_add_new(t_token **token_list, char *input)
+{
+    
+}
