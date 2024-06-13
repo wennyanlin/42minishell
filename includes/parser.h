b4a30285a1c5e5702306ef31dd53fc3332fe32b4
Agustín Lanusse <lanusseagustin@gmail.com>
2024-06-13 23:42:01 +0200

    header

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: aglanuss <aglanuss@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 17:44:59 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/06/11 12:54:02 by aglanuss         ###   ########.fr       */
+/*   Updated: 2024/06/13 14:12:12 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,27 @@ typedef struct	s_command
 	struct s_command	*next;
 }								t_command;
 
+/* DELETE THIS */
+typedef enum e_token
+{
+	PIPE = 1,
+	GREAT,
+	GREAT_GREAT,
+	LESS,
+	LESS_LESS
+}	t_token;
+
+typedef struct s_lexer
+{
+	char					*value;
+	t_token				type;
+	struct s_lexer	*prev;
+	struct s_lexer	*next;
+}	t_lexer;
+/* DELETE THIS */
+
 t_command	*new_command_node(char **str);
 void			add_command_node(t_command **commands_lst, t_command *new_command);
+int count_words_until_pipe(t_lexer **lexer);
 
 #endif
