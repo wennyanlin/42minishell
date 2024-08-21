606a452574f60e3e0b1e0613f7f3964aa32200b6
Wenyan Lin <wennyan.lin@gmail.com>
2024-08-21 16:28:57 +0200

    Replaced printf with ft_putstr_fd

--- src/parser/parser_syntax.c
+++ src/parser/parser_syntax.c
@@ -6,7 +6,7 @@
 /*   By: wlin <wlin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 18:17:03 by wlin              #+#    #+#             */
-/*   Updated: 2024/08/15 22:49:41 by wlin             ###   ########.fr       */
+/*   Updated: 2024/08/21 12:03:46 by wlin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,27 @@
 int	prompt_error_message(t_metachar type)
 {
 	if (type == PIPE)
-		printf("minishell: syntax error near unexpected token `%c'\n", C_PIPE);
+		ft_putstr_fd("minishell: syntax error near unexpected token `|'\n", 2);
 	else if (type == LESS)
-		printf("minishell: syntax error near unexpected token `%c'\n", C_LESS);
+		ft_putstr_fd("minishell: syntax error near unexpected token `<'\n", 2);
 	else if (type == LESS_LESS)
-		printf("minishell: syntax error near unexpected token `%c%c'\n", C_LESS,
-			C_LESS);
+		ft_putstr_fd("minishell: syntax error near unexpected token `<<'\n", 2);
 	else if (type == GREAT)
-		printf("minishell: syntax error near unexpected token `%c'\n", C_GREAT);
+		ft_putstr_fd("minishell: syntax error near unexpected token `>'\n", 2);
 	else if (type == GREAT_GREAT)
-		printf("minishell: syntax error near unexpected token`%c%c'\n", C_GREAT,
-			C_GREAT);
+		ft_putstr_fd("minishell: syntax error near unexpected token`>>'\n", 2);
 	return (EXIT_FAILURE);
 }
 
 int	check_pipe_error(t_token *token_lst)
 {
-	if (token_lst->prev == NULL || token_lst->next == NULL || token_lst->next->metachar == PIPE)
-		return (prompt_error_message(PIPE));
+	if (token_lst->prev == NULL || token_lst->next == NULL
+		|| token_lst->next->metachar == PIPE)
+			return (prompt_error_message(PIPE));
 	else if (is_redirection(token_lst->next->metachar)
 		&& token_lst->next->next == NULL)
-		return (printf("minishell: syntax error near unexpected "
-				"token`newline'\n"), EXIT_FAILURE);
+		return (ft_putstr_fd("minishell: syntax error near unexpected "
+				"token`newline'\n", STDERR_FILENO), EXIT_FAILURE);
 	else if (token_lst->prev == NULL && token_lst->next->word
 		&& token_lst->next->next == NULL)
 		return (prompt_error_message(PIPE));
@@ -47,8 +46,8 @@ int	check_pipe_error(t_token *token_lst)
 int	check_redirection_error(t_token *token_lst)
 {
 	if (token_lst->next == NULL)
-		return (printf("minishell: syntax error near unexpected "
-				"token`newline'\n"), EXIT_FAILURE);
+		return (ft_putstr_fd("minishell: syntax error near unexpected "
+				"token`newline'\n", STDERR_FILENO), EXIT_FAILURE);
 	else if (token_lst->next->metachar == PIPE)
 		return (prompt_error_message(PIPE));
 	else if (is_redirection(token_lst->next->metachar)
