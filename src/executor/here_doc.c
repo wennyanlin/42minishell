092274304cc1e8162c84b068f0997cadc7fa0089
Wenyan Lin <wennyan.lin@gmail.com>
2024-09-30 18:15:49 +0200

    Added NULL protection check

--- src/executor/here_doc.c
+++ src/executor/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: wlin <wlin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 19:15:28 by wlin              #+#    #+#             */
-/*   Updated: 2024/09/30 18:04:08 by wlin             ###   ########.fr       */
+/*   Updated: 2024/09/30 18:12:11 by wlin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,12 @@ char	*create_heredoc_filename(void)
 	filename_prefix = "/tmp/tmp_heredoc";
 	filename = ft_strdup(filename_prefix);
 	i = 0;
-	while (access(filename, F_OK) == 0)
+	while (filename != NULL && access(filename, F_OK) == 0)
 	{
 		free(filename);
 		filename_suffix = ft_itoa(i);
+		if (filename_suffix == NULL)
+			return (NULL);
 		filename = ft_strjoin(filename_prefix, filename_suffix);
 		free(filename_suffix);
 		i++;
@@ -55,6 +57,8 @@ char	*read_here_doc(t_data *data, char *delimiter)
 	int		hd_fd;
 
 	filename = create_heredoc_filename();
+	if (filename == NULL)
+		exit_minishell(data, "heredoc", strerror(errno), errno);
 	hd_fd = open(filename, O_CREAT | O_WRONLY, 0666);
 	if (hd_fd == INVALID)
 		exit_minishell(data, filename, strerror(errno), errno);
